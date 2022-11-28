1d694a96933da6e711f3bd6e961bf8161ad26731
Anilchauhan1995 <poojavargla1995@gmail.com>
2022-11-28 11:18:16 +0530

    Adding file for test purposes only.

---
+++ assets/js/custom.js
@@ -0,0 +1,11 @@
+
+$(".slick-slider").slick({
+  slidesToShow: 4,
+  infinite:true,
+  arrows: true,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000
+    // dots: false, Boolean
+   // arrows: false, Boolean
+ });
